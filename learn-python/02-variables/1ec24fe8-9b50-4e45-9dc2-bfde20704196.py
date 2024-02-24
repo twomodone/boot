a809f33450f4846f141f14da8a87f30d78857d2f
twomodone <twomodone@gmail.com>
2024-02-24 22:09:39 +0200

    Add 1ec24fe8-9b50-4e45-9dc2-bfde20704196 solution

---
+++ learn-python/02-variables/1ec24fe8-9b50-4e45-9dc2-bfde20704196.py
@@ -0,0 +1,10 @@
+sentence_start = "You have "
+sentence_end = " health"
+
+player1_health = "1200"
+player2_health = "1100"
+
+# Don't edit above this line
+
+print(sentence_start + player1_health + sentence_end)
+print(sentence_start + player2_health + sentence_end)
