77551d2ea33d25fde44f00dec7278fca06ec7ff6
twomodone <twomodone@gmail.com>
2024-02-24 22:35:44 +0200

    Add 0f4fa755-1ce7-468b-bf34-c1460e97bf28 solution

---
+++ learn-python/03-functions/0f4fa755-1ce7-468b-bf34-c1460e97bf28.py
@@ -0,0 +1,17 @@
+def to_celsius(f):
+    celsius = 5/9 * (f - 32)
+    return celsius
+
+
+## Don't touch below this line
+
+
+def test(f):
+    c = round(to_celsius(f), 2)
+    print(f, "degrees fahrenheit is", c, "degrees celsius")
+
+
+test(100)
+test(88)
+test(104)
+test(112)
