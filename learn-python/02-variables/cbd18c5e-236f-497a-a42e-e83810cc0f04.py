b2e7647bc0dd15eba1796d3ef51130ac7aa2561e
twomodone <twomodone@gmail.com>
2024-02-24 22:12:29 +0200

    Add cbd18c5e-236f-497a-a42e-e83810cc0f04 solution

---
+++ learn-python/02-variables/cbd18c5e-236f-497a-a42e-e83810cc0f04.py
@@ -0,0 +1,22 @@
+game_one_score = 97
+game_two_score = 91
+game_three_score = 106
+game_four_score = 105
+game_five_score = 96
+game_six_score = 93
+game_seven_score = 104
+
+# Don't touch above this line
+
+average_score = (
+    game_one_score + 
+    game_two_score + 
+    game_three_score + 
+    game_four_score + 
+    game_five_score + 
+    game_six_score + 
+    game_seven_score) / 7 
+
+# Don't touch below this line
+
+print(round(average_score))
