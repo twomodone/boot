97454db4c7138d7d7c5ead30750d3e2744aa20a6
twomodone <twomodone@gmail.com>
2024-02-24 22:13:09 +0200

    Add 16120c56-cfaf-48e7-8fef-b2b1128c05e2 solution

---
+++ learn-python/02-variables/16120c56-cfaf-48e7-8fef-b2b1128c05e2.py
@@ -0,0 +1,14 @@
+part_one = "Roses are red, "
+part_two = "violets are blue."
+part_three = "Python is cool, "
+part_four = "and so are you!"
+
+# Don't touch above this line
+
+line_one = part_one + part_two 
+line_two = part_three + part_four
+
+# Don't touch below this line
+
+print(line_one)
+print(line_two)
