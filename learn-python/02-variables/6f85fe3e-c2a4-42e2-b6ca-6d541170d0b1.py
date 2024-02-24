97f423f993377c9edd899b1a188477022c9e45bf
twomodone <twomodone@gmail.com>
2024-02-24 21:46:02 +0200

    Add 6f85fe3e-c2a4-42e2-b6ca-6d541170d0b1 solution

---
+++ learn-python/02-variables/6f85fe3e-c2a4-42e2-b6ca-6d541170d0b1.py
@@ -0,0 +1,7 @@
+player_health = 100
+
+player_has_magic = True
+
+# don't touch below this line
+print(f"player_health is a/an {type(player_health)}")
+print(f"player_has_magic is a/an {type(player_has_magic)}")
