8de75869192dff7c035f5d43145e07955abf6e8c
twomodone <twomodone@gmail.com>
2024-02-24 21:36:44 +0200

    Add f12268f9-e9a5-4fdc-98d4-444e5cfa0247 solution

---
+++ learn-python/02-variables/f12268f9-e9a5-4fdc-98d4-444e5cfa0247.py
@@ -0,0 +1,17 @@
+player_health = 1000
+
+# reduce by 100 here
+player_health = player_health - 100
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
