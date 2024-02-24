0918845e73af808dd9efe960caee1a9a851aea52
twomodone <twomodone@gmail.com>
2024-02-24 22:10:50 +0200

    Add e4fac74c-9d67-41ad-a85c-c579cb3ad76f solution

---
+++ learn-python/02-variables/e4fac74c-9d67-41ad-a85c-c579cb3ad76f.py
@@ -0,0 +1,11 @@
+email_start = "Thank you for shopping with us!"
+email_middle = "Your total today was: "
+email_end = "Please consider filling out our customer survey."
+dollar_sign = "$"
+total_price = "20.25"
+
+# don't touch above this line
+
+print(email_start)
+print(email_middle + dollar_sign + total_price)
+print(email_end)
