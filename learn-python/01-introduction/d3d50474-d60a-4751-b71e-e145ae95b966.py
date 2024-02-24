6952984d6116530625eb3a2845a58babb85cb242
twomodone <twomodone@gmail.com>
2024-02-24 21:10:30 +0200

    Add d3d50474-d60a-4751-b71e-e145ae95b966 solution

---
+++ learn-python/01-introduction/d3d50474-d60a-4751-b71e-e145ae95b966.py
@@ -0,0 +1,8 @@
+sword_damage = 10
+player_health = 100
+health_after_attack = player_health - sword_damage
+
+# Don't touch below this line
+print(f"Lollilfred's health is: {player_health}")
+print(f"Lollilfred is hit by a sword for {sword_damage} damage...")
+print(f"Lollilfred's health is now: {health_after_attack}")
