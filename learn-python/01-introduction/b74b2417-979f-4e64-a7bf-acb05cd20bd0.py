e80d43a5b281d752bb0ae05a807c4cb6e66fb674
twomodone <twomodone@gmail.com>
2024-02-24 21:32:22 +0200

    Add b74b2417-979f-4e64-a7bf-acb05cd20bd0 solution

---
+++ learn-python/01-introduction/b74b2417-979f-4e64-a7bf-acb05cd20bd0.py
@@ -0,0 +1,12 @@
+items_in_cart = 5
+cost_of_each_item = 2.50
+total_cost = items_in_cart * cost_of_each_item
+
+# Don't Touch Above This line
+
+print ("Thank you for shopping at Bootmart!")
+print ("Your total today was:")
+
+# Don't Touch Below This Line
+
+print(f"${total_cost:.2f}")
