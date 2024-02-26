ed1e8f39122733efe7e4c8d89097c965456f0d16
twomodone <twomodone@gmail.com>
2024-02-26 23:23:01 +0200

    Add eb147649-719f-4946-8edd-a8ee5578b9fd solution

---
+++ learn-python/09-lists/eb147649-719f-4946-8edd-a8ee5578b9fd/main.py
@@ -0,0 +1,12 @@
+def contains_leather_scraps(items):
+    found = False
+
+    # don't touch above this line
+
+    for item in items:
+        if item == "Leather Scraps":
+            found = True
+
+    # don't touch below this line
+
+    return found
---
+++ learn-python/09-lists/eb147649-719f-4946-8edd-a8ee5578b9fd/test.py
@@ -0,0 +1,51 @@
+from main import *
+
+run_cases = [
+    (["Potion", "Healing Potion", "Iron Breastplate", "Leather Scraps"], True),
+    (["Potion", "Shortsword", "Buckler", "Iron Mace"], False),
+]
+
+submit_cases = run_cases + [
+    ([], False),
+    (["Leather Scraps"], True),
+    (["Potion", "Leather Scraps", "Leather Scraps"], True),
+    (["Potion", "Healing Potion"], False),
+    (["Leather scraps"], False),
+    (["Leather", "Scraps"], False),
+]
+
+
+def test(input1, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {input1}")
+    print(f"Expecting: {expected_output}")
+    result = contains_leather_scraps(input1)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
