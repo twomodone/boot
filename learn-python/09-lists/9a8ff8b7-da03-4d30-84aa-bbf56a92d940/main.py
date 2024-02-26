2ac277aac096898f50592c073335fb4bb94d5be9
twomodone <twomodone@gmail.com>
2024-02-26 22:30:58 +0200

    Add 9a8ff8b7-da03-4d30-84aa-bbf56a92d940 solution

---
+++ learn-python/09-lists/9a8ff8b7-da03-4d30-84aa-bbf56a92d940/main.py
@@ -0,0 +1,12 @@
+def get_leather_scraps():
+    inventory = [
+        "Healing Potion",
+        "Leather Scraps",
+        "Iron Helmet",
+        "Bread",
+        "Shortsword",
+    ]
+
+    item_index = 1
+
+    return inventory[item_index]
---
+++ learn-python/09-lists/9a8ff8b7-da03-4d30-84aa-bbf56a92d940/test.py
@@ -0,0 +1,44 @@
+from main import *
+
+run_cases = [
+    ("Leather Scraps",),
+]
+
+submit_cases = run_cases + [
+    ("Leather Scraps",),
+]
+
+
+def test(expected_output):
+    print("---------------------------------")
+    print(f"Expecting: {expected_output}")
+    result = get_leather_scraps()
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
