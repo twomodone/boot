8d228710fd2f93d7780abb1c15b9fdac6b6b68a8
twomodone <twomodone@gmail.com>
2024-02-27 01:58:44 +0200

    Add 48981250-b1bb-4a49-b1e4-c4134341f125 solution

---
+++ learn-python/10-dictionaries/48981250-b1bb-4a49-b1e4-c4134341f125/main.py
@@ -0,0 +1,30 @@
+def calculate_total(items_purchased, grocery_list):
+    item_prices = {
+        "milk": 2.50,
+        "eggs": 3.25,
+        "bread": 1.21,
+        "cheese": 3.50,
+        "apples": 7.44,
+        "bananas": 3.88,
+        "carrots": 3.89,
+        "lettuce": 1.12,
+        "potatoes": 32.21,
+        "cereal": 5.99,
+    }
+
+    # Don't touch above this line
+
+    unpurchased_items = []
+    receipt = {}
+    total_cost = 0
+
+    for item in grocery_list:
+        if item not in items_purchased:
+            unpurchased_items.append(item)
+
+    for item in items_purchased:
+        price = item_prices[item]
+        receipt[item] = price
+        total_cost += price
+
+    return unpurchased_items, receipt, total_cost
