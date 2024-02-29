7b2c9cdb8276b40c60b361649c359ef6daf8bb78
twomodone <twomodone@gmail.com>
2024-02-29 13:57:03 +0200

    Add fa8322cb-4631-4f3e-aa27-328f1b68d703 solution

---
+++ learn-object-oriented-programming/02-encapsulation/fa8322cb-4631-4f3e-aa27-328f1b68d703.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.__courses = {}
+
+    def calculate_letter_grade(self, score):
+        if score >= 90:
+            return "A"
+        elif score >= 80 and score <= 89:
+            return "B"
+        elif score >= 70 and score <= 79:
+            return "C"
+        elif score >= 60 and score <= 69:
+            return "D"
+        else:
+            return "F"
+
+    def add_course(self, course_name, score):
+        letter_grade = self.calculate_letter_grade(score)
+        self.__courses[course_name] = letter_grade
+
+    def get_courses(self):
+        return self.__courses
