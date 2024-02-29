a3613bdd2453cd646cf2192af13efbbe9f08ddba
twomodone <twomodone@gmail.com>
2024-02-29 14:58:59 +0200

    Add 34866c1e-cd76-4e53-a7d4-b891fb40970a solution

---
+++ learn-object-oriented-programming/03-abstraction/34866c1e-cd76-4e53-a7d4-b891fb40970a/main.py
@@ -0,0 +1,43 @@
+import random
+
+
+class DeckOfCards:
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+
+    def __init__(self):
+        self.__cards = []
+        self.create_deck()
+
+    def create_deck(self):
+        for suit in DeckOfCards.SUITS:
+            for rank in DeckOfCards.RANKS:
+                self.__cards.append((rank, suit))
+
+    def shuffle_deck(self):
+        random.shuffle(self.__cards)
+
+    def deal_card(self):
+        if len(self.__cards) == 0:
+            return None
+        else:
+            return self.__cards.pop()
+
+    # don't touch below this line
+
+    def __str__(self):
+        return f"The deck has {len(self.__cards)} cards"
