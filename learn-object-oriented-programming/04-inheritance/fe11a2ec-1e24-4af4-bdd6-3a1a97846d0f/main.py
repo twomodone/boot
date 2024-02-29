0055f1bcb439e78084a925c9d90fc1826d21d374
twomodone <twomodone@gmail.com>
2024-02-29 17:05:45 +0200

    Add fe11a2ec-1e24-4af4-bdd6-3a1a97846d0f solution

---
+++ learn-object-oriented-programming/04-inheritance/fe11a2ec-1e24-4af4-bdd6-3a1a97846d0f/main.py
@@ -0,0 +1,26 @@
+class Hero:
+    def __init__(self, name, health):
+        self.__name = name
+        self.__health = health
+
+    def get_name(self):
+        return self.__name
+
+    def get_health(self):
+        return self.__health
+
+    def take_damage(self, damage):
+        self.__health -= damage
+
+
+class Archer(Hero):
+    def __init__(self, name, health, num_arrows):
+        super().__init__(name, health)
+        self.__number_of_arrows = num_arrows
+
+    def shoot(self, target):
+        if self.__number_of_arrows == 0:
+            raise Exception("not enough arrows")
+        else:
+            self.__number_of_arrows -= 1
+            target.take_damage(10)
